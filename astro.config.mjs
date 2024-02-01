aef4d56d34abac234826299cee91e36f58ccd12b
Sejmou <samo.kolter@gmail.com>
2024-02-01 21:37:16 +0100

    add tailwind, basic page layout (subject to change)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [tailwind(), mdx(), sitemap(), icon()]
 });
